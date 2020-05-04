5d4491dde2793e291c19ebb34ce5bdb554f71cb9
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-04 13:12:07 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,4 +1,11 @@
 # Use this space to code along with the readme
+def say_hello_world_ten_times(phrase, i)
+  i.times do
+    puts phrase
+  end
+end
+
+say_hello_world_ten_times(phrase, 10)
 
 phrase = "Hello World!"
 puts phrase
